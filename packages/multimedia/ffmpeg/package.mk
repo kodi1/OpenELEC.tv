69493ba1fc20ecb014ad644a81e1a559069472a2
kodi1 <kodi.dev.one@gmail.com>
2015-12-23 11:45:01 +0200

    Update ffmpeg 2.6.6

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="ffmpeg"
-PKG_VERSION="2.6.4"
+PKG_VERSION="2.6.8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="LGPLv2.1+"
